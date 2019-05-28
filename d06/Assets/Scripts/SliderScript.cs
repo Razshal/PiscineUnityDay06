c7e4756efd0233a26effcfa95d4d0f50914aa5ad
Razshal <fontenille.mathieu@gmail.com>
2019-05-28 15:06:41 +0200

    ex01 - done

--- d06/Assets/Scripts/SliderScript.cs
+++ d06/Assets/Scripts/SliderScript.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SliderScript : MonoBehaviour {
     private Slider slider;
     public bool isIncreased;
     public float decraseValue = 0.1f;
+    public AudioSource alarm;
+    private bool alarmPlaying = false;
 
     // Use this for initialization
     void Start () {
@@ -17,12 +20,24 @@ public class SliderScript : MonoBehaviour {
     {
         if (slider.value < slider.maxValue)
             slider.value += val;
+        if (!alarmPlaying && slider.value >= 75)
+        {
+            alarmPlaying = true;
+            alarm.Play();
+        }
+        if (alarmPlaying && slider.value < 75)
+        {
+            alarmPlaying = false;
+            alarm.Stop();
+        }
+        if (slider.value >= 100)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     private void Update()
     {
         if (!isIncreased && slider.value > slider.minValue)
-            slider.value -= decraseValue;
+            IncreaseDetection(-decraseValue);
 
         if (Input.GetKey(KeyCode.LeftShift)
             && (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0))
