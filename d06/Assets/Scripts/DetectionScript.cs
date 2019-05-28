16b0adbc29ecd0ac5107d5bff70c4c7f686ec9e4
Razshal <fontenille.mathieu@gmail.com>
2019-05-28 14:08:11 +0200

    ex01 - almost done

---
+++ d06/Assets/Scripts/DetectionScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectionScript : MonoBehaviour {
+    private SliderScript sliderScript;
+    public float detectionZoneValue = 0.1f;
+
+    private void Start()
+	{
+        sliderScript = GameObject.Find("Slider").GetComponent<SliderScript>();
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+        sliderScript.IncreaseDetection(detectionZoneValue);
+        sliderScript.isIncreased = true;
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+        sliderScript.isIncreased = false;
+	}
+}
---
+++ d06/Assets/Scripts/SliderScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour {
+    private Slider slider;
+    public bool isIncreased;
+    public float decraseValue = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        slider = gameObject.GetComponent<Slider>();
+	}
+
+    public void IncreaseDetection(float val)
+    {
+        if (slider.value < slider.maxValue)
+            slider.value += val;
+    }
+
+	private void Update()
+	{
+        if (!isIncreased && slider.value > slider.minValue)
+            slider.value -= decraseValue;
+	}
+}
