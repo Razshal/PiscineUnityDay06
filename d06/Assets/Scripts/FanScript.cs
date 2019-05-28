52200d8bbf29b9ab071513f46ba6163c2e0b08cc
Razshal <fontenille.mathieu@gmail.com>
2019-05-28 19:37:24 +0200

    ex04 - wip

---
+++ d06/Assets/Scripts/FanScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanScript : MonoBehaviour {
+    public DetectionScript detectionScript;
+    public GameObject particles;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            detectionScript.detectionZoneValue = 0.2f;
+            particles.SetActive(true);
+        }
+	}
+}
